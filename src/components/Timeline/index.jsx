903a4c0499ef8b902507637c8e07fc5d5eb8d378
Martin Makarsky <martinmakarsky@gmail.com>
2020-07-31 11:31:54 +0200

    Tune timeline styling.

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -28,6 +28,11 @@ const timeline = () => {
                   period {
                     value
                   }
+                  is_current {
+                    value {
+                      codename
+                    }
+                  }
                 }
               }
             }
@@ -39,6 +44,7 @@ const timeline = () => {
 
   const timelineItems = data.kontentItemTimeline.elements.timeline_items.value
 
+
   return (
     <div>
       <h2>My journey</h2>
@@ -47,9 +53,11 @@ const timeline = () => {
           {timelineItems &&
             timelineItems.map(item => (
               <div className="entry" key={item.system.codename}>
-                <div className="title big">{item.elements.period.value}</div>
+                <div className={item.elements.is_current.value[0].codename === "true" ? "title big" : "title small"}>{item.elements.period.value}</div>
                 <div className="timeline_body">
-                  <p>{item.elements.title.value}</p>
+                  <p className="timeline_body__title">{item.elements.title.value}</p>
+                  <p className="timeline_body__company">{item.elements.company.value}</p>
+                  <p className="timeline_body__location">{item.elements.location.value}</p>
                 </div>
               </div>
               ))
