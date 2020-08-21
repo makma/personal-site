c843c3b8cff36099a73b35bf55c0542de6e8f1cd
Martin Makarsky <martinmakarsky@gmail.com>
2020-08-21 16:10:49 +0200

    Change domain.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const lost = require('lost')
 const pxtorem = require('postcss-pxtorem')
 
-const url = 'https://gatsby-starter-kontent-lumen.netlify.com/'
+const url = 'https://martinmakarsky.com/'
 
 require('dotenv').config()
 
