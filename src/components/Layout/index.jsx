064a5a4c394439680d82940b427f4d0655e1f556
Martin Makarsky <martinmakarsky@gmail.com>
2020-08-22 12:01:47 +0200

    Fork me corner.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
+import ForkMeOnGithub from '../ForkMeOnGithub/index'
 
 class Layout extends React.Component {
   render() {
     const { children } = this.props
 
     return (
-      <div className="layout">
-        <Helmet
-          defaultTitle="Blog by Martin Makarsky"
-          htmlAttributes= {{lang: 'en'}} 
-        >
-          <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-        </Helmet>
-        {children}
-      </div>
+      <>
+        <div className="layout">
+          <Helmet
+            defaultTitle="Blog by Martin Makarsky"
+            htmlAttributes={{ lang: 'en' }}
+          >
+            <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+          </Helmet>
+          {children}
+        </div>
+        <ForkMeOnGithub
+          repo="https://github.com/makma/martinmakarsky.com"
+          colorBackground="black"
+          colorOctocat="white"
+        />
+      </>
     )
   }
 }
