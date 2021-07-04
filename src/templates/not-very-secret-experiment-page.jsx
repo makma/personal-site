4ea800692131a0c79e75d009a9d6de6cc92171cd
Martin Makarský <martinmakarsky@gmail.com>
2021-07-04 15:42:45 +0200

    add botd to experiment page

--- src/templates/not-very-secret-experiment-page.jsx
+++ src/templates/not-very-secret-experiment-page.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import CodeHighlighter from '../components/CodeHighlighter/index'
 import fpOS from '@fingerprintjs/fingerprintjs'
 import fpPro from '@fingerprintjs/fingerprintjs-pro'
+import Botd from '@fpjs-incubator/botd-agent'
 
 const NotVerySecretExperimentPage = () => {
   const [visitorIdOS, setVisitorIdOS] = useState('Waiting for visitorIdOS...')
   const [resultOS, setResultOS] = useState('')
+  const [botdResult, setBotdResult] = useState('')
 
   const [visitorIdPro, setVisitorIdPro] = useState(
     'Waiting for visitorIdPro...'
@@ -13,6 +15,19 @@ const NotVerySecretExperimentPage = () => {
   const [resultPro, setResultPro] = useState('')
 
   useEffect(() => {
+    // Initialize an agent at application startup.
+    const botdPromise = Botd.load({
+      token: 'RBKN12LFJAyyjMSr30J',
+      mode: 'allData',
+    })
+
+    ;(async () => {
+      // Get the bot detection result when you need it.
+      const botd = await botdPromise
+      const result = await botd.detect()
+      setBotdResult(JSON.stringify(result, null, 2))
+    })()
+
     const fpOSPromise = fpOS.load()
 
     ;(async () => {
@@ -37,6 +52,10 @@ const NotVerySecretExperimentPage = () => {
 
   return (
     <>
+      <div>
+        <h2>botd</h2>
+        <CodeHighlighter language="json" code={botdResult} />
+      </div>
       <div>
         <h2>FingerprintJS open source</h2>
         <h3>{visitorIdOS}</h3>
